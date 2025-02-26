4b5de5fd1722ea2a23561694dc71a2a2c975c719
solopigeon02 <samuel.briant@studytafensw.edu.au>
2025-02-26 12:31:51 +1100

    f26 second

---
+++ script.js
@@ -0,0 +1,41 @@
+/* for (let i = 1; i <= 100; i++) {
+    if (i % 3 === 0 &&  i % 5 === 0) {
+        console.log("FizzBuzz");
+    }
+    else if (i % 3 === 0) {
+        console.log("Fizz");
+    }
+    else if (i % 5 === 0) {
+        console.log("Buzz");
+    }
+    else {
+        console.log(i);
+    }
+}; */
+
+
+/* let mark = 70;
+
+if (mark > 0 || mark < 100) {
+    if (mark < 60) {
+        console.log("your grade is 'F'.");
+    }
+    else if (mark < 70) {
+        console.log("your grade is 'D'.");
+    }
+    else if (mark < 80) {
+        console.log("your grade is 'C'.");
+    }
+    else if (mark < 90) {
+        console.log("your grade is 'B'.");
+    }
+    else {
+        console.log("your grade is 'A'.");
+    }
+} */
+
+//let x = [1,2,3,4,5,6],
+
+for (let x = [1,2,3,4,5,6],) {
+    console.log();
+};
